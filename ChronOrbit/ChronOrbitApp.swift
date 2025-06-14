8b7549197411fcd4db58005984540029619bdbc1
YourNickname <kuromugi0715@gmail.com>
2025-06-15 00:41:05 +0900

    Initial Commit

---
+++ ChronOrbit/ChronOrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronOrbitApp.swift
+//  ChronOrbit
+//
+//  Created by 黒本誉隆 on 2025/06/15.
+//
+
+import SwiftUI
+
+@main
+struct ChronOrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronOrbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChronOrbit
+//
+//  Created by 黒本誉隆 on 2025/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
