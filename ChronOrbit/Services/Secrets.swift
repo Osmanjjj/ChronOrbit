a69bd5d73d340a3bf13222df2e11e8c62c303f42
YourNickname <kuromugi0715@gmail.com>
2025-06-15 02:12:25 +0900

    初コミット

--- ChronOrbit/Services/Secrets.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Secrets {
-    // あなたのプロジェクト URL と anonKey を差し替えてください
-    static let supabaseURL = URL(string: "https://tkxwnklqamdqjzouizih.supabase.co")!
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRreHdua2xxYW1kcWp6b3VpemloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTE2ODMsImV4cCI6MjA2NTIyNzY4M30.iv5Iid-1XBBkl3_n4OdRaC_CbdQfEauOuRlkVC9My-Y"
-}
